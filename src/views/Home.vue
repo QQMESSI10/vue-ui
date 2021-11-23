60f20f9845fb2f5c7fe6f7ca8b53dae658abcf18
qinxiang <15163281896@163.com>
2021-11-23 17:34:37 +0800

    添加首页和文档页面

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="top-nav">
+      <div class="logo">LOGO</div>
+      <div class="menu">
+        <div>菜单一</div>
+        <div>菜单二</div>
+      </div>
+    </div>
+    <div class="banner">
+      <h1>阿祥UI</h1>
+      <h2>一个厉害的UI框架</h2>
+      <p class="actions">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.top-nav {
+  background-color: aquamarine;
+}
+</style>
